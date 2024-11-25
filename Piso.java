9d4060deee02ddd700d3d546d6e7d2cb53cf36ea
JuaaanCalopino <juan.calopino@unl.edu.ec>
2024-11-25 01:03:30 -0500

    Delete Piso.java

--- Piso.java
+++
@@ -1,9 +0,0 @@
-public class Piso {
-    private int cantidadPlazas;
-    private int nivel;
-
-    public Piso(int nivel, int cantidadPlazas) {
-        this.nivel = nivel;
-        this.cantidadPlazas = cantidadPlazas;
-    }
-}
